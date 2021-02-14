652847d7639cae4fb530a615b8adbe4a5f04c1c5
liqiongchao-home <liqiongchao2016@gmail.com>
2021-02-14 09:07:02 +0800

    StringTable GCTest

--- src/main/java/com/chaocode/jvm/atguigu/chapter13/java3/StringGCTest.java
+++ src/main/java/com/chaocode/jvm/atguigu/chapter13/java3/StringGCTest.java
@@ -3,6 +3,9 @@ package com.chaocode.jvm.atguigu.chapter13.java3;
 /**
  * String的垃圾回收:
  * -Xms15m -Xmx15m -XX:+PrintStringTableStatistics -XX:+PrintGCDetails
+ * 空方法体的时候，字符串常量池的个数（Number of entries）
+ * time=100的时候，字符串常量池的个数
+ * time=100000的时候，字符串常量池的个数
  *
  * @author shkstart  shkstart@126.com
  * @create 2020  21:27
