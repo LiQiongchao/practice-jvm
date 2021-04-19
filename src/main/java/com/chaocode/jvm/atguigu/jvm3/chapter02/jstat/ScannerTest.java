0613ec312d5817df3d629c84a43d6afa026ad2e6
liqiongchao-home <liqiongchao2016@gmail.com>
2021-04-19 21:47:12 +0800

    性能调控代码

---
+++ src/main/java/com/chaocode/jvm/atguigu/jvm3/chapter02/jstat/ScannerTest.java
@@ -0,0 +1,15 @@
+package com.chaocode.jvm.atguigu.jvm3.chapter02.jstat;
+
+import java.util.Scanner;
+
+/**
+ * @author shkstart
+ * @create 14:57
+ *
+ */
+public class ScannerTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String info = scanner.next();
+    }
+}
